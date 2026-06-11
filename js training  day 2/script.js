b0044a8ffb17b18f40288930d4fbad6e3c86cb5a
Saurav Giri <realanmup@gmail.com>
2026-06-11 20:33:32 +0545

    old update

--- js training  day 2/script.js
+++ js training  day 2/script.js
@@ -1,6 +1,6 @@
 // this is comment
-
-localStorage
+// stores data on browser
+// localStorage.getItem
 // variables
 let todos = [];
 
@@ -9,9 +9,33 @@ let inputTodo = document.getElementById('todo');
 let todoListUL = document.querySelector('ul');
 
 
+function trimedInput() {
+    return inputTodo.value.trim()
+}
+
+
+function isValid() {
+    
+    if(trimedInput() == "") {
+        inputTodo.classList.add("errorInput")
+        return false;
+    } else { 
+        inputTodo.classList.remove("errorInput")
+        return true;
+    }
+}
+
+
 function submit() {
-    let inputValue = inputTodo.value
-    todos.push(inputValue)
+    // form validation
+    if (!isValid()) {
+        return;
+    }
+    
+    todos.push(trimedInput())
+    updateDataOnStorage()
+    
+
     inputTodo.value = ""
     render();
 }
@@ -41,7 +65,8 @@ function renderItem(data, index) {
 
     deleteBtn.onclick = function(index) {
         todos.splice(index, 1)
-        alert('ok');
+        // alert('ok');
+        updateDataOnStorage()
         render();
     }
 
@@ -52,10 +77,57 @@ function renderItem(data, index) {
         let updatedValue = prompt(`Edit ? + ${item.target.textContent}`)
         debugger
         
-        if (!(updatedValue == null || updatedValue == '' || updatedValue == item.target.textContent)) {
-            todos[index] = updatedValue;
-        }
+        if (updatedValue == null || updatedValue == '' || updatedValue == item.target.textContent)
+            return;
+
+        todos[index] = updatedValue;
+        updateDataOnStorage()
         render()
     })
     return liTag
+}
+
+
+document.addEventListener("DOMContentLoaded", init())
+
+function init() {
+    updateDay();
+    showClock();
+    loadData();
+}
+
+function updateDay() {
+    const startDate = new Date('2026-06-06')
+    const today = new Date() 
+    const diffinMS = Math.abs(today - startDate)
+    const diffInDays = Math.floor(diffinMS / (1000*60*60*24))
+    const dayCountSpan = document.getElementById('dayCount')
+    dayCountSpan.textContent = diffInDays
+}
+
+const colorPallate = ["purple", "aqua", "#1F51FF", "red", "skyblue"];
+
+function showClock() {
+    const h2Tag = document.querySelector("h2")
+
+    
+    setInterval(function() {
+        h2Tag.textContent = new Date().toLocaleTimeString();
+        const randomValue = Math.floor(Math.random()*10);
+        
+
+        h2Tag.style.color = colorPallate[colorPallate.length%randomValue]
+    }, 1000)
+}
+
+
+function loadData() {
+    let data = localStorage.getItem('todos')
+    todos = JSON.parse(data)
+    render()
+}
+
+function updateDataOnStorage() {
+    // save on db (capacity: 10MB)
+    localStorage.setItem('todos', JSON.stringify(todos))
 }
